75f5d746adca23cf04aaf3c495912faf8c817d64
Mark Ruisdael <ruisdaeless@yahoo.com>
2021-09-13 18:37:16 +0400

    Start scripting

--- js/chrome-range.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("#range").oninput = function() {
-    this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
-};
